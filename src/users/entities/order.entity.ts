1bc07b9c140298f24cd10c6d86f34f34989c3893
john102594 <johnalbertrf@gmail.com>
2022-01-14 19:06:28 -0500

    lasted

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,8 +1,30 @@
-import { User } from './user.entity';
-import { Product } from './../../products/entities/product.entity';
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+  Entity,
+} from 'typeorm';
 
+import { Customer } from './customer.entity';
+import { OrderItem } from './order-item.entity';
+// import { Product } from './../../products/entities/product.entity';
+
+@Entity()
 export class Order {
-  date: Date;
-  user: User;
-  products: Product[];
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updateAt: Date;
+
+  @ManyToOne(() => Customer, (customer) => customer.orders)
+  customer: Customer;
+
+  @OneToMany(() => OrderItem, (orderitem) => orderitem.order)
+  items: OrderItem[];
 }
